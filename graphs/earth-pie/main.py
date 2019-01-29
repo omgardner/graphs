bd8df37d8c29badad029eeaa420017728b378749
Oscar Gardner <oscarmgardner@gmail.com>
2019-01-29 23:22:19 +1100

    Creates the video now, but had to uninvert each image, and every image after the first is bad

--- graphs/earth-pie/main.py
+++ graphs/earth-pie/main.py
@@ -1,6 +1,12 @@
 import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import cv2
+from glob import glob
 
 def sector_mask(shape,centre,radius,angle_range):
+    # I HAVE TODO THIS MYSELF... i will not understand otherwaise
+
     """
     Return a boolean mask for a circular sector. The start/stop angles in  
     `angle_range` should be given in clockwise order.
@@ -10,21 +16,24 @@ def sector_mask(shape,centre,radius,angle_range):
     x,y = np.ogrid[:shape[0],:shape[1]]
     cx,cy = centre
     tmin,tmax = np.deg2rad(angle_range)
-
+    print(angle_range)
     # ensure stop angle > start angle
     if tmax < tmin:
-            tmax += 2*np.pi
+        tmax += 2*np.pi
 
     # convert cartesian --> polar coordinates
     r2 = (x-cx)*(x-cx) + (y-cy)*(y-cy)
     theta = np.arctan2(x-cx,y-cy) - tmin
-
+    
     # wrap angles between 0 and 2*pi
     theta %= (2*np.pi)
 
     # circular mask
     circmask = r2 <= radius*radius
 
+    #THIS IS A PROBLEM.. 
+    # likely anglemask or theta has the wrong value?
+    # what changes in a loop?
     # angular mask
     anglemask = theta <= (tmax-tmin)
 
@@ -33,44 +42,60 @@ def sector_mask(shape,centre,radius,angle_range):
 
 def gen_start_angle_ranges(n):
     # asign equal angle range to each file via generator
+    # `offset` rotates all angle ranges by `offset` degrees
+    # lots of modulo, TODO check if removal is necessary
     angle = 360 / n
     for i in range(n-1):
         yield angle*i, angle*(i+1)
     else:
         # ensure that final sector ends at 360 degrees,
         # in case of rounding error
-        yield angle*(n-1), 360  
+        yield angle*(n-1), 360
 
 
+def create_frame(img_matrices, offset):
+    n = len(img_matrices)
+    full_matrix = None
 
-from matplotlib import pyplot as plt
-import seaborn as sns
-import cv2
-from glob import glob
-
-filepaths = glob("data/test/*.png")
-
-n = len(filepaths)
-fig, ax = plt.subplots()
-
-main_matrix = None
-for fp, angle_range in zip(filepaths, gen_start_angle_ranges(n)):
-    # individual masked image sector
-    matrix = cv2.imread(fp)
-    width, height = matrix.shape[:2]
-    # radius is width // 2. This is an assumption that height ~= width, radius ~= width
-    mask = sector_mask(matrix.shape, (width//2, height//2), width//2, angle_range)
-    matrix[~mask] = 0
-
-    if main_matrix is None:
-        main_matrix = matrix
-    else:
-        # not checked for efficiency. is this index based?
-        main_matrix = np.where(main_matrix != 0, main_matrix, matrix)
+    angle_ranges = [(a+offset, b+offset)  for a,b in gen_start_angle_ranges(n)]
     
-# plot
-ax.imshow(main_matrix) # test
-sns.despine(ax=ax, left=True, bottom=True)
-ax.set_yticks([])
-ax.set_xticks([])
-plt.show()
+    for i, (matrix, angle_range) in enumerate(zip(img_matrices, angle_ranges)):
+        
+        # individual masked image sector
+        width, height = matrix.shape[:2]
+        # radius is width // 2. This is an assumption that height ~= width, radius ~= width
+        mask = sector_mask(matrix.shape, (width//2, height//2), width//2, angle_range)
+        matrix[~mask] = 0
+        if full_matrix is None:
+            full_matrix = matrix
+        else:
+            # not checked for efficiency. is this index based?
+            full_matrix = np.where(matrix == 0, full_matrix, matrix)
+
+    # TEMPORARY INVERSION until i debug...
+    return cv2.bitwise_not(full_matrix) # uninvert image... WHERE DID IT BECOME INVERTED????
+
+
+
+def main():
+    filepaths = glob("data/test/*.png")
+    # avoids reloading the image from fp every loop
+    img_matrices = [cv2.imread(fp) for fp in filepaths]
+
+    FPS = 4
+    VIDEO_SHAPE = (640,640)
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    video = cv2.VideoWriter('data/test/video.mp4', fourcc, FPS, VIDEO_SHAPE, 1)
+    for offset in range(0, 360, 45):
+        frame = cv2.resize(create_frame(img_matrices, offset), VIDEO_SHAPE)
+        
+        cv2.imwrite(f"data/test/res/{offset}.png", frame)
+        video.write(cv2.resize(create_frame(img_matrices, offset), VIDEO_SHAPE))
+        print(f"Frame created for offset {offset} degrees.")
+        
+    video.release()
+
+
+if __name__ == '__main__':
+    main()
